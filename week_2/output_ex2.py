25c52e8b714a67dfe5bc503ccfa70eea871314c5
meher-un-nisa <mehersalim2@outlook.com>
2024-06-01 23:48:04 -0500

    Delete week_2 directory

--- week_2/output_ex2.py
+++
@@ -1,8 +0,0 @@
-""" 
-    Title: output_ex2.py
-    Author: Professor Krasso
-    Date: 11 August 2022
-    Description: Example file for outputting values.
-"""
-
-print('Python is fun to learn!!!')
--- week_2/output_ex4.py
+++
@@ -1,9 +0,0 @@
-""" 
-    Title: output_ex4.py
-    Author: Professor Krasso
-    Date: 11 August 2022
-    Description: Example file for outputting values.
-"""
-
-print('My name is {0}'.format('Professor Krasso'))
-print('My favorite course to teach is {0}'.format('WEB 450 - Mastering the MEAN Stack!'))
