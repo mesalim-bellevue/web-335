eeed142bc7c6f48c32b3a2b6cb0b3f538702bc72
meher-un-nisa <mehersalim2@outlook.com>
2024-06-09 12:09:18 -0500

    Add files via upload

---
+++ week_2/quote.py
@@ -0,0 +1,9 @@
+"""
+Author: Meher Salim
+Date: 06/08/2024
+File Name: quote.py
+Description: Build a simple Python program that prints my favorite quote
+"""
+
+#print quote by Walt Disney
+print("If you can dream it, you can do it. - Walt Disney")
